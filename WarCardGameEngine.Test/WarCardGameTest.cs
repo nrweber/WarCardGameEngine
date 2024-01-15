4b4240daf1be6be2fb409bea94c984465bac01a3
Nic Weber <nrweber@gmail.com>
2024-01-15 12:19:50 -0600

    working save

--- WarCardGameEngine.Test/WarCardGameTest.cs
+++ WarCardGameEngine.Test/WarCardGameTest.cs
@@ -10,4 +10,350 @@ public class UnitTest1
         StandardPokerDeck testDeck =  new(StartingStates.Empty);
         Assert.Throws<ArgumentException>(() => new WarCardGame(testDeck));
     }
+    
+    [Fact]
+    public void ThrowsArgumentExceptionIfDeckContainsOneCard()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        Assert.Throws<ArgumentException>(() => new WarCardGame(testDeck));
+    }
+    
+    [Fact]
+    public void DefaultConstructor_EachPlayerHas26Cards_GameStateIsWaitingForBothPlayers()
+    {
+        WarCardGame game = new();
+
+        Assert.Equal(26, game.PlayerOneDeckSize);
+        Assert.Equal(26, game.PlayerTwoDeckSize);
+
+        Assert.Equal(WarCardGame.GameState.WaitingForBothPlayers, game.State);
+    }
+
+    [Fact]
+    public void PlayerOneFlipCard_CardIsPutIntoPlayList_GameStateIsWaitingOnPlayerTwo()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Spade, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Heart, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Ace));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerOneFlipCard();
+
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Equal(new Card(Suits.Club, Values.Ace), game.PlayerOnePlayedCards[0]);
+        Assert.Equal(WarCardGame.GameState.WaitingForPlayerTwo, game.State);
+    }
+
+    [Fact]
+    public void PlayerOneFlipCard_SecondCallWhileWaitingForPlayerTwoDoesNothing()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Spade, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Heart, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Ace));
+
+        WarCardGame game = new(testDeck);
+
+        //First Call
+        game.PlayerOneFlipCard();
+
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Equal(new Card(Suits.Club, Values.Ace), game.PlayerOnePlayedCards[0]);
+        Assert.Equal(WarCardGame.GameState.WaitingForPlayerTwo, game.State);
+       
+        //Second Call
+        game.PlayerOneFlipCard();
+
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Equal(WarCardGame.GameState.WaitingForPlayerTwo, game.State);
+    }
+    
+    [Fact]
+    public void PlayerTwoFlipCard_CardIsPutIntoPlayList_GameStateIsWaitingOnPlayerOne()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Spade, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Heart, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Ace));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerTwoFlipCard();
+
+        Assert.Single(game.PlayerTwoPlayedCards);
+        Assert.Equal(new Card(Suits.Spade, Values.Ace), game.PlayerTwoPlayedCards[0]);
+        Assert.Equal(WarCardGame.GameState.WaitingForPlayerOne, game.State);
+    }
+
+    [Fact]
+    public void PlayerTwoFlipCard_SecondCallWhileWaitingForPlayerOneDoesNothing()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Spade, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Heart, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Ace));
+
+        WarCardGame game = new(testDeck);
+
+        // First Call
+        game.PlayerTwoFlipCard();
+
+        Assert.Single(game.PlayerTwoPlayedCards);
+        Assert.Equal(new Card(Suits.Spade, Values.Ace), game.PlayerTwoPlayedCards[0]);
+        Assert.Equal(WarCardGame.GameState.WaitingForPlayerOne, game.State);
+        
+        // Second Call
+        game.PlayerTwoFlipCard();
+
+        Assert.Single(game.PlayerTwoPlayedCards);
+        Assert.Equal(WarCardGame.GameState.WaitingForPlayerOne, game.State);
+    }
+ 
+    [Fact]
+    public void PlayerOneFlipCard_CanBeCalledAfterPlayerTwoFlipCard()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerTwoFlipCard();
+
+        game.PlayerOneFlipCard();
+
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Equal(new Card(Suits.Club, Values.Ace), game.PlayerOnePlayedCards[0]);
+    }
+
+    [Fact]
+    public void PlayerTwoFlipCard_CanBeCalledAfterPlayerOneFlipCard()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerOneFlipCard();
+
+        game.PlayerTwoFlipCard();
+
+        Assert.Single(game.PlayerTwoPlayedCards);
+        Assert.Equal(new Card(Suits.Club, Values.Two), game.PlayerTwoPlayedCards[0]);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void AfterBothPlay_PlayerOneHigher_GameStateIsPlayerOneWinsRound(bool oneFirst)
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        if(oneFirst)
+        {
+            game.PlayerOneFlipCard();
+            game.PlayerTwoFlipCard();
+        }
+        else
+        {
+            game.PlayerTwoFlipCard();
+            game.PlayerOneFlipCard();
+        }
+
+        Assert.Equal(WarCardGame.GameState.PlayerOneWinsRound, game.State);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void AfterBothPlay_PlayerTwoHigher_GameStateIsPlayerOneWinsRound(bool oneFirst)
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        if(oneFirst)
+        {
+            game.PlayerOneFlipCard();
+            game.PlayerTwoFlipCard();
+        }
+        else
+        {
+            game.PlayerTwoFlipCard();
+            game.PlayerOneFlipCard();
+        }
+
+        Assert.Equal(WarCardGame.GameState.PlayerTwoWinsRound, game.State);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void AfterBothPlay_CardsMatch_GameStateIsWar(bool oneFirst)
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Spade, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        if(oneFirst)
+        {
+            game.PlayerOneFlipCard();
+            game.PlayerTwoFlipCard();
+        }
+        else
+        {
+            game.PlayerTwoFlipCard();
+            game.PlayerOneFlipCard();
+        }
+
+        Assert.Equal(WarCardGame.GameState.WarWaitingForBothPlayers, game.State);
+    }
+
+    [Fact]
+    public void AfterPlayerOneWinsRound_PlayerOneFlipCard_and_PlayerTwoFlipCard_DoNothing()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerOneFlipCard();
+        game.PlayerTwoFlipCard();
+
+        Assert.Equal(WarCardGame.GameState.PlayerOneWinsRound, game.State);
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Single(game.PlayerTwoPlayedCards);
+
+        // Try to flip another card
+        game.PlayerOneFlipCard();
+        game.PlayerTwoFlipCard();
+        
+        Assert.Equal(WarCardGame.GameState.PlayerOneWinsRound, game.State);
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Single(game.PlayerTwoPlayedCards);
+    }
+    
+    [Fact]
+    public void AfterPlayerTwoWinsRound_PlayerOneFlipCard_and_PlayerTwoFlipCard_DoNothing()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerOneFlipCard();
+        game.PlayerTwoFlipCard();
+
+        Assert.Equal(WarCardGame.GameState.PlayerTwoWinsRound, game.State);
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Single(game.PlayerTwoPlayedCards);
+
+        // Try to flip another card
+        game.PlayerOneFlipCard();
+        game.PlayerTwoFlipCard();
+        
+        Assert.Equal(WarCardGame.GameState.PlayerTwoWinsRound, game.State);
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Single(game.PlayerTwoPlayedCards);
+    }
+
+    [Fact]
+    public void AfterPlayerOneWinsRound_RestRound_MovesAllPlayedCardsToPlayerOne_StateBacktoWaitingForBothPlayers()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerOneFlipCard();
+        game.PlayerTwoFlipCard();
+
+        Assert.Equal(WarCardGame.GameState.PlayerOneWinsRound, game.State);
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Single(game.PlayerTwoPlayedCards);
+
+        game.ResetRound();
+
+        Assert.Equal(WarCardGame.GameState.WaitingForBothPlayers, game.State);
+        Assert.Empty(game.PlayerOnePlayedCards);
+        Assert.Empty(game.PlayerTwoPlayedCards);
+        Assert.Equal(3, game.PlayerOneDeckSize);
+        Assert.Equal(1, game.PlayerTwoDeckSize);
+    }
+
+    [Fact]
+    public void AfterPlayerTwoWinsRound_RestRound_MovesAllPlayedCardsToPlayerTwo_StateBacktoWaitingForBothPlayers()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Club, Values.Four));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerOneFlipCard();
+        game.PlayerTwoFlipCard();
+
+        Assert.Equal(WarCardGame.GameState.PlayerTwoWinsRound, game.State);
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Single(game.PlayerTwoPlayedCards);
+
+        game.ResetRound();
+
+        Assert.Equal(WarCardGame.GameState.WaitingForBothPlayers, game.State);
+        Assert.Empty(game.PlayerOnePlayedCards);
+        Assert.Empty(game.PlayerTwoPlayedCards);
+        Assert.Equal(1, game.PlayerOneDeckSize);
+        Assert.Equal(3, game.PlayerTwoDeckSize);
+    }
+
+    [Fact]
+    public void WarWaitingOnBothPlayers_PlayerOneFlipCard_TwoMoreCardsAreFlipped_StateGoesToWarWaitingForPlayerTwo()
+    {
+        StandardPokerDeck testDeck =  new(StartingStates.Empty);
+        testDeck.PushBottom(new Card(Suits.Club, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Spade, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Heart, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Ace));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Two));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Three));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Four));
+        testDeck.PushBottom(new Card(Suits.Diamond, Values.Five));
+
+        WarCardGame game = new(testDeck);
+        game.PlayerOneFlipCard();
+        game.PlayerTwoFlipCard();
+
+        Assert.Single(game.PlayerOnePlayedCards);
+        Assert.Single(game.PlayerTwoPlayedCards);
+        Assert.Equal(WarCardGame.GameState.WarWaitingForBothPlayers, game.State);
+        
+        game.PlayerOneFlipCard();
+        Assert.Equal(3, game.PlayerOnePlayedCards.Count);
+    }
 }
--- WarCardGameEngine/WarCardGame.cs
+++ WarCardGameEngine/WarCardGame.cs
@@ -10,7 +10,74 @@ namespace nic_weber.WarCardGameEngine;
 /// </summary>
 public class WarCardGame
 {
-    private readonly StandardPokerDeck _deck;
+    public enum GameState
+    {
+        WaitingForBothPlayers,
+        WaitingForPlayerOne,
+        WaitingForPlayerTwo,
+        PlayerOneWinsRound,
+        PlayerTwoWinsRound,
+        WarWaitingForBothPlayers,
+    };
+
+
+
+    /// <summary>
+    /// Current state of the game
+    /// </summary>
+    public GameState State { get; private set; } = GameState.WaitingForBothPlayers;
+
+    /// <summary>
+    /// Number of cards left in player one's deck
+    /// </summary>
+    public int PlayerOneDeckSize 
+    { 
+        get 
+        {
+            return _playerOneDeck.CardsInDeck;
+        }
+    }
+
+    /// <summary>
+    /// Cards played by player one
+    /// </summary>
+    public IReadOnlyList<Card> PlayerOnePlayedCards { get => _playerOnePlayedCards.AsReadOnly(); }
+    
+    /// <summary>
+    /// Number of cards left in player two's deck
+    /// </summary>
+    public int PlayerTwoDeckSize 
+    { 
+        get 
+        {
+            return _playerTwoDeck.CardsInDeck;
+        }
+    }
+
+    /// <summary>
+    /// Cards played by player two
+    /// </summary>
+    public IReadOnlyList<Card> PlayerTwoPlayedCards { get => _playerTwoPlayedCards.AsReadOnly(); }
+    
+
+
+    private StandardPokerDeck _playerOneDeck = new(StartingStates.Empty);
+    private StandardPokerDeck _playerTwoDeck = new(StartingStates.Empty);
+    private List<Card> _playerOnePlayedCards = new();
+    private List<Card> _playerTwoPlayedCards = new();
+
+
+
+    /// <summary>
+    /// Start a new game of War with a standard 52 card deck.
+    /// The deck is evenly devided between each player
+    /// one card at a time.
+    /// </summary>
+    public WarCardGame()
+    {
+        DealCards(new StandardPokerDeck(StartingStates.Standard52));
+    }
+
 
     /// <summary>
     /// Start a new game of War with the given deck.
@@ -24,9 +91,151 @@ public class WarCardGame
     /// </exception>
     public WarCardGame(StandardPokerDeck deck)
     {
-        if(deck.CardsInDeck == 0)
+        if(deck.CardsInDeck <= 1)
             throw new ArgumentException("Deck needs to have at least 2 cards.");
 
-        _deck = deck;
+        DealCards(deck);
+    }
+
+
+    /// <summary>
+    /// Place top card from player one's deck on the played card list
+    /// </summary>
+    public void PlayerOneFlipCard()
+    {
+        switch(State)
+        {
+            case GameState.WaitingForBothPlayers:
+                playerOnePlayCard();
+                State = GameState.WaitingForPlayerTwo;
+                break;
+            case GameState.WaitingForPlayerOne:
+                playerOnePlayCard();
+                checkRound();
+                break;
+        }
+    }
+
+
+    /// <summary>
+    /// Place top card from player two's deck on the played card list
+    /// </summary>
+    public void PlayerTwoFlipCard()
+    {
+        switch(State)
+        {
+            case GameState.WaitingForBothPlayers:
+                playerTwoPlayCard();
+                State = GameState.WaitingForPlayerOne;
+                break;
+            case GameState.WaitingForPlayerTwo:
+                playerTwoPlayCard();
+                checkRound();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// After a player has one the round, moves the played cards
+    /// to the bottom of the winning players deck and starts
+    /// a new round.
+    /// </summary>
+    public void ResetRound()
+    {
+        switch(State)
+        {
+            case GameState.PlayerOneWinsRound:
+                foreach(var c in _playerOnePlayedCards)
+                    _playerOneDeck.PushBottom(c);
+                foreach(var c in _playerTwoPlayedCards)
+                    _playerOneDeck.PushBottom(c);
+                _playerOnePlayedCards.Clear();
+                _playerTwoPlayedCards.Clear();
+                State = GameState.WaitingForBothPlayers;
+                break; 
+            case GameState.PlayerTwoWinsRound:
+                foreach(var c in _playerOnePlayedCards)
+                    _playerTwoDeck.PushBottom(c);
+                foreach(var c in _playerTwoPlayedCards)
+                    _playerTwoDeck.PushBottom(c);
+                _playerOnePlayedCards.Clear();
+                _playerTwoPlayedCards.Clear();
+                State = GameState.WaitingForBothPlayers;
+                break; 
+        }
+         
+    }
+
+
+    private void DealCards(StandardPokerDeck deck)
+    {
+        while(deck.CardsInDeck > 1)
+        {
+            _playerOneDeck.PushBottom(deck.Pop()!);
+            _playerTwoDeck.PushBottom(deck.Pop()!);
+        }
+        if(deck.CardsInDeck == 1)
+        {
+            _playerOneDeck.PushBottom(deck.Pop()!);
+        }
+    }
+
+
+    private void playerOnePlayCard()
+    {
+        var card = _playerOneDeck.Pop();
+        if(card is not null)
+        {
+            _playerOnePlayedCards.Add(card);
+        }
+    }
+
+
+    private void playerTwoPlayCard()
+    {
+        var card = _playerTwoDeck.Pop();
+        if(card is not null)
+        {
+            _playerTwoPlayedCards.Add(card);
+        }
+    }
+   
+
+    private void checkRound()
+    {
+        if(_playerTwoPlayedCards.Count == 0 || _playerOnePlayedCards.Count == 0)
+            return;
+
+        int p1 = CardValue(_playerOnePlayedCards.Last());
+        int p2 = CardValue(_playerTwoPlayedCards.Last());
+
+        if(p1 > p2)
+            State = GameState.PlayerOneWinsRound;
+        else if(p1 < p2)
+            State = GameState.PlayerTwoWinsRound;
+        else
+            State = GameState.WarWaitingForBothPlayers;
+    }
+
+
+    private int CardValue(Card card)
+    {
+        return card.Value switch
+        {
+            Values.Two => 2,
+            Values.Three => 3,
+            Values.Four => 4,
+            Values.Five => 5,
+            Values.Six => 6,
+            Values.Seven => 7,
+            Values.Eight => 8,
+            Values.Nine => 9,
+            Values.Ten => 10,
+            Values.Jack => 11,
+            Values.Queen => 12,
+            Values.King => 13,
+            Values.Ace => 14,
+            _ => 0 //Jokers should not be in here but lets just make them low
+        };
     }
 }
